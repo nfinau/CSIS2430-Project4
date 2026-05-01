2f1b28ff609b7b9a514096fffbd425fae1df33d6
nfinau <omgitznaomi@gmail.com>
2026-04-30 20:26:18 -0600

    Add Simulation class with core loop and landing tracking

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,1 +1,37 @@
+import java.util.Arrays;
 
+public class Simulation {
+
+    private int totalTurns;
+    private Player player;
+    private Dice dice;
+    private int[] landingCounts;
+
+    public Simulation(int totalTurns) {
+        this.totalTurns = totalTurns;
+        this.player = new Player();
+        this.dice = new Dice();
+        this.landingCounts = new int[40];
+    }
+
+    public void run() {
+        for (int i = 0; i < totalTurns; i++) {
+            takeTurn();
+        }
+    }
+
+    private void takeTurn() {
+        int roll = dice.roll();
+        player.move(roll);
+
+        int position = player.getPosition();
+        landingCounts[position]++;
+    }
+
+    public void printResults() {
+        System.out.println("Landing Frequencies:");
+        for (int i = 0; i < landingCounts.length; i++) {
+            System.out.println("Space " + i + ": " + landingCounts[i]);
+        }
+    }
+}
