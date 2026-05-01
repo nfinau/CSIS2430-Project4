ce810e4f17e38fc49019d23fe5538324d249f127
nfinau <omgitznaomi@gmail.com>
2026-04-30 20:26:36 -0600

    Add Player class with movement and position tracking

--- src/Player.java
+++ src/Player.java
@@ -1,1 +1,16 @@
+public class Player {
 
+    private int position;
+
+    public Player() {
+        this.position = 0;
+    }
+
+    public void move(int spaces) {
+        position = (position + spaces) % 40;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
