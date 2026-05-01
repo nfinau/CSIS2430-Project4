372f95aec6e65eff8f5b603d1b53672f838f83ec
nfinau <omgitznaomi@gmail.com>
2026-04-30 20:28:15 -0600

    Implement doubles logic, jail system, and go-to-jail behavior

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Simulation {
 
     private int totalTurns;
@@ -21,11 +19,59 @@ public class Simulation {
     }
 
     private void takeTurn() {
-        int roll = dice.roll();
-        player.move(roll);
 
-        int position = player.getPosition();
-        landingCounts[position]++;
+        int doublesCount = 0;
+
+        if (player.isInJail()) {
+            handleJailTurn();
+            landingCounts[player.getPosition()]++;
+            return;
+        }
+
+        do {
+            int roll = dice.roll();
+
+            if (dice.isDouble()) {
+                doublesCount++;
+            } else {
+                doublesCount = 0;
+            }
+
+            if (doublesCount == 3) {
+                player.sendToJail();
+                landingCounts[player.getPosition()]++;
+                return;
+            }
+
+            player.move(roll);
+
+            // Go To Jail square (index 30)
+            if (player.getPosition() == 30) {
+                player.sendToJail();
+                landingCounts[player.getPosition()]++;
+                return;
+            }
+
+        } while (dice.isDouble());
+
+        landingCounts[player.getPosition()]++;
+    }
+
+    private void handleJailTurn() {
+
+        int roll = dice.roll();
+
+        if (dice.isDouble()) {
+            player.releaseFromJail();
+            player.move(roll);
+        } else {
+            player.incrementJailTurn();
+
+            if (player.getJailTurns() >= 3) {
+                player.releaseFromJail();
+                player.move(roll);
+            }
+        }
     }
 
     public void printResults() {
