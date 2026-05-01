144592113d5c4c5ee63321ece66cbefda44867e8
nfinau <omgitznaomi@gmail.com>
2026-04-30 20:27:45 -0600

    Add jail state tracking to Player class

--- src/Player.java
+++ src/Player.java
@@ -1,9 +1,13 @@
 public class Player {
 
     private int position;
+    private boolean inJail;
+    private int jailTurns;
 
     public Player() {
         this.position = 0;
+        this.inJail = false;
+        this.jailTurns = 0;
     }
 
     public void move(int spaces) {
@@ -12,5 +16,28 @@ public class Player {
 
     public int getPosition() {
         return position;
+    }
+
+    public void sendToJail() {
+        position = 10; // Jail index
+        inJail = true;
+        jailTurns = 0;
+    }
+
+    public boolean isInJail() {
+        return inJail;
+    }
+
+    public void incrementJailTurn() {
+        jailTurns++;
+    }
+
+    public int getJailTurns() {
+        return jailTurns;
+    }
+
+    public void releaseFromJail() {
+        inJail = false;
+        jailTurns = 0;
     }
 }
