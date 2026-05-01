788132d0bc1be7deaddead33923e1970f2365d71
nfinau <omgitznaomi@gmail.com>
2026-04-30 20:26:50 -0600

    Add Dice class with two-dice roll logic

--- src/Dice.java
+++ src/Dice.java
@@ -1,1 +1,16 @@
+import java.util.Random;
 
+public class Dice {
+
+    private Random random;
+
+    public Dice() {
+        random = new Random();
+    }
+
+    public int roll() {
+        int die1 = random.nextInt(6) + 1;
+        int die2 = random.nextInt(6) + 1;
+        return die1 + die2;
+    }
+}
