32d4e170175753b1d0a1b19611b2aa03ed2867aa
nfinau <omgitznaomi@gmail.com>
2026-04-30 20:32:03 -0600

    Add Monopoly board space names

--- src/Board.java
+++ src/Board.java
@@ -6,6 +6,53 @@ public class Board {
     public static final int JAIL = 10;
     public static final int GO_TO_JAIL = 30;
 
+    private static final String[] SPACE_NAMES = {
+        "GO",
+        "Mediterranean Avenue",
+        "Community Chest",
+        "Baltic Avenue",
+        "Income Tax",
+        "Reading Railroad",
+        "Oriental Avenue",
+        "Chance",
+        "Vermont Avenue",
+        "Connecticut Avenue",
+        "Jail / Just Visiting",
+        "St. Charles Place",
+        "Electric Company",
+        "States Avenue",
+        "Virginia Avenue",
+        "Pennsylvania Railroad",
+        "St. James Place",
+        "Community Chest",
+        "Tennessee Avenue",
+        "New York Avenue",
+        "Free Parking",
+        "Kentucky Avenue",
+        "Chance",
+        "Indiana Avenue",
+        "Illinois Avenue",
+        "B&O Railroad",
+        "Atlantic Avenue",
+        "Ventnor Avenue",
+        "Water Works",
+        "Marvin Gardens",
+        "Go To Jail",
+        "Pacific Avenue",
+        "North Carolina Avenue",
+        "Community Chest",
+        "Pennsylvania Avenue",
+        "Short Line Railroad",
+        "Chance",
+        "Park Place",
+        "Luxury Tax",
+        "Boardwalk"
+    };
+
+    public static String getSpaceName(int position) {
+        return SPACE_NAMES[position];
+    }
+
     public static boolean isChance(int position) {
         return position == 7 || position == 22 || position == 36;
     }
